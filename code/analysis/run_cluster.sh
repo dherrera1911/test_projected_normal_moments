abf7f2a3dab465d11c175a25103859aa9312dbd4
dherresp-adm <dherrera1911@gmail.com>
2024-05-10 17:15:06 -0400

    Fix slurm script

--- code/analysis/run_cluster.sh
+++ code/analysis/run_cluster.sh
@@ -1,17 +1,16 @@
-#!/bin/bash                                                                         
-#SBATCH --job-name=motion_depth                                                     
-#SBATCH --ntasks=1                                                                  
-#SBATCH --mem=12G                                                                    
-#SBATCH --time=00:60:00                                                             
-#SBATCH --mail-type=ALL                                                             
-#SBATCH --output=script_run.out                                                
-#SBATCH --gres=gpu:1 # Can be gpu:p100:1 or gpu:a100:1                              
-#SBATCH --partition=normal                                                          
-#SBATCH --qos=cpu                                                                   
-                                                                                    
-# Initialize conda for the Bash shell                                               
+#!/bin/bash
+#SBATCH --job-name=prnorm
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=10G
+#SBATCH --time=04:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --output=script_run.out
+#SBATCH --partition=besteffort
+#SBATCH --qos=besteffort
+
+# Initialize conda for the Bash shell
 #python ./01_3d_approximation.py
 #python ./02_nD_approximation.py
 python ./03_3d_moment_matching.py
-
 
